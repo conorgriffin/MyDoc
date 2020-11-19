8417a9c056c4d0a2431a2bbacdcf19fc712ce0e4
Conor Griffin <conorgriffin@gmail.com>
2020-11-19 09:33:36 +0000

    Initial Commit

---
+++ MyDoc/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MyDoc
+//
+//  Created by Conor Griffin on 19/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Binding var document: MyDocDocument
+
+    var body: some View {
+        TextEditor(text: $document.text)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(document: .constant(MyDocDocument()))
+    }
+}
---
+++ MyDoc/MyDocApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDocApp.swift
+//  MyDoc
+//
+//  Created by Conor Griffin on 19/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct MyDocApp: App {
+    var body: some Scene {
+        DocumentGroup(newDocument: MyDocDocument()) { file in
+            ContentView(document: file.$document)
+        }
+    }
+}
